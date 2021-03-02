f9f4f88d4d9f0f2234332a81dd94011218bd018f
cpusillo <cpusillo@gmail.com>
2021-03-02 14:22:22 -0500

    api views update

--- api/models.py
+++ api/models.py
@@ -9,6 +9,9 @@ def generate_unique_code():
     while True:
         # Generate a random code, all uppercase with a length of 6 as declared above
         code = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+    return code
 
 # Create your models here.
 class Room(models.Model):
